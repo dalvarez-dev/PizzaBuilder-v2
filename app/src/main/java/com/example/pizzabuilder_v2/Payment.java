2a689ab3f44ebd70db608d34fd8f4ab72c3ac4f0
BroadBandV <bigdannyt102@gmail.com>
2023-12-06 15:00:24 -0500

    UPDATED ADDRESS VALIDATION: now works perfectly, lets gooooooo!

--- app/src/main/java/com/example/pizzabuilder_v2/Payment.java
+++ app/src/main/java/com/example/pizzabuilder_v2/Payment.java
@@ -2,6 +2,8 @@ package com.example.pizzabuilder_v2;
 
 
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
@@ -16,6 +18,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,6 +29,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 import java.util.Scanner;
 
 public class Payment extends AppCompatActivity {
@@ -33,13 +38,13 @@ public class Payment extends AppCompatActivity {
 
     TextView subtotaldata;
 
-    EditText addressEditText;
+    EditText addressInput;
 
-    TextView addressValidationMessage;
+    TextView validationMessage;
 
-    Handler addressvalidationHandler;
+    private static final String API_KEY = "AIzaSyBRiOJkdzKIKHBRdBrJ63Hn0t7xsq1dn40";
 
-    Runnable addressValidatioRunnable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,103 +52,82 @@ public class Payment extends AppCompatActivity {
         setContentView(R.layout.payment);
 
         subtotaldata = findViewById(R.id.subtotaldata);
+        addressInput = findViewById(R.id.given_address_edit_text);
+        validationMessage = findViewById(R.id.addressValidationMessage);
 
-        addressEditText = findViewById(R.id.given_address_edit_text);
-        addressValidationMessage = findViewById(R.id.addressValidationMessage);
-
-        addressvalidationHandler = new Handler();
-        addressValidatioRunnable = this::validateAddress;
-
-        addressEditText.addTextChangedListener(new TextWatcher() {
+        Button validateButton = findViewById(R.id.validateButton);
+        validateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                addressvalidationHandler.removeCallbacks(addressValidatioRunnable);
-
-                addressvalidationHandler.postDelayed(addressValidatioRunnable, 5000);
+            public void onClick(View v) {
+                validateAddress(addressInput.getText().toString());
             }
         });
 
+
     }
 
-    private void validateAddress(){
-        String address= addressEditText.getText().toString();
-        new GeocodingTask().execute(address);
-    }
-
-    private class GeocodingTask extends AsyncTask<String, Void, Boolean> {
-        protected Boolean doInBackground(String... addresses) {
-            if(addresses.length == 0 || addresses[0].trim().isEmpty()) return false;
-
-            String address = addresses[0].trim();
-            String apiKey = "AIzaSyBRiOJkdzKIKHBRdBrJ63Hn0t7xsq1dn40";
-
-            try{
-                URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key" + apiKey);
-
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                try {
-                    InputStream in = urlConnection.getInputStream();
-                    Scanner sc = new Scanner(in);
-                    sc.useDelimiter("\\A");
-
-                    if(sc.hasNext()) {
-                        String response = sc.next();
-                        JSONObject jsonObject = new JSONObject(response);
-                        JSONArray results = jsonObject.getJSONArray("results");
-
-                        if(results.length() > 0) {
-                            JSONArray addressComponents = results.getJSONObject(0).getJSONArray("address_components");
-
-                            boolean hasStreet = false;
-                            boolean hasCity = false;
-
-                            for(int i = 0; i < addressComponents.length(); i++){
-                                JSONObject component = addressComponents.getJSONObject(i);
-                                JSONArray types = component.getJSONArray("types");
-
-                                for(int j = 0; j< types.length(); j++){
-                                    String type = types.getString(j);
-
-                                    if(type.equals("street_number") || type.equals("route")){
-                                        hasStreet= true;
-
-                                        }
-                                    else if(type.equals("locality")){
-                                        hasCity = true;
-                                    }
-                                    }
-                                }
-                            return hasStreet && hasCity;
-                        }
-                        }
-                    return false;
-                } finally {
-                    urlConnection.disconnect();
-                }
-            } catch (IOException | JSONException e){
-                e.printStackTrace();
-                return false;
+    private void validateAddress(String userAddress){
+        Geocoder geocoder = new Geocoder(this);
+        try{
+            List<Address> addresses = geocoder.getFromLocationName(userAddress, 1);
+            if(addresses.isEmpty()){
+                Toast.makeText(this, "Address not found, please enter a valid address", Toast.LENGTH_SHORT).show();
             }
-        }
 
-        protected void onPostExecute(Boolean isValid){
-            if(isValid){
-                addressValidationMessage.setText("Address valid");
+            Address address = addresses.get(0);
+            double lat = address.getLatitude();
+            double lon = address.getLongitude();
+
+            List<Address> reverseGeo = geocoder.getFromLocation(lat, lon, 1);
+
+            if(reverseGeo.isEmpty()){
+                Toast.makeText(this, "Error, Could not validate address", Toast.LENGTH_SHORT).show();
+            }
+
+            Address reverseAddy = reverseGeo.get(0);
+
+            String makeStandardized = getStandardizedAddress(reverseAddy);
+
+            if (userAddress.equalsIgnoreCase(makeStandardized)){
+                validationMessage = findViewById(R.id.addressValidationMessage);
+                validationMessage.setText("Address is Valid");
             } else{
-                addressValidationMessage.setText("Please enter a valid address");
+                validationMessage = findViewById(R.id.addressValidationMessage);
+                validationMessage.setText("Address invalid, you might have meant: " + makeStandardized);
+                addressInput.setText(makeStandardized);
             }
+        } catch (IOException e){
+            e.printStackTrace();
+            Toast.makeText(this, "Error occured while trying to validate address", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private String getStandardizedAddress(Address address){
+        StringBuilder stringBuilder = new StringBuilder();
+        String addressL1 = address.getAddressLine(0);
+        String addressL2 = address.getAddressLine(1);
+        String locality = address.getLocality();
+        String adminArea = address.getAdminArea();
+        String postalCode = address.getPostalCode();
+
+        if (addressL1 != null){
+            stringBuilder.append(addressL1);
+        }
+        if(addressL2 != null){
+            stringBuilder.append(", ").append(addressL2);
+        }
+        if(locality != null){
+            stringBuilder.append(", ").append(locality);
+        }
+        if(adminArea != null){
+            stringBuilder.append(", ").append(adminArea);
+        }
+        if(postalCode != null){
+            stringBuilder.append(", ").append(postalCode);
+        }
+        return stringBuilder.toString().trim();
+    }
+
     }
 
 
@@ -153,4 +137,4 @@ public class Payment extends AppCompatActivity {
 
 
 
-}//end class
+//end class
